8c55197c40a55d21657c88d10d1a6bce458f2e9c
aragaoiberis <aragaoiberis@gmail.com>
2025-07-19 11:18:26 -0300

    Commit

--- Cargo/src/main.rs
+++ Cargo/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
-    println!("println! chama um Macro, se fosse uma função seria println (sem o !)");
+    println!("println! chama um Macro. Se fosse uma função seria println (sem o !)");
 }
 
 
